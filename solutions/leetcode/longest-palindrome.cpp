05e320f9255803201139582d0433536e44a6d121
Krish Matta <self@krishxmatta.dev>
2022-05-18 23:57:56 -0400

    Solve leetcode/longest-palindrome

---
+++ solutions/leetcode/longest-palindrome.cpp
@@ -0,0 +1,56 @@
+using ll = long long;
+using db = long double;
+using str = string;
+
+using vi = vector<int>;
+#define pb push_back
+#define all(x) begin(x), end(x)
+#define sz(x) (int)(x).size()
+
+using pi = pair<int,int>;
+#define f first
+#define s second
+#define mp make_pair
+
+#define FOR(i, a, b) for (int i = (a); i < (b); ++i)
+#define F0R(i, a) FOR(i, 0, a)
+#define ROF(i, a, b) for (int i = (b) - 1; i >= (a); --i)
+#define R0F(i, a) ROF(i, 0, a)
+#define rep(a) F0R(_, a)
+#define each(a, x) for (auto& a: x)
+
+const int dr[4] = {1, 0, -1, 0}, dc[4] = {0, 1, 0, -1};
+
+const int MOD = 1e9+7;
+const int INF = 1e9;
+
+void setIO(string name = "") {
+	cin.tie(0)->sync_with_stdio(0);
+  if (sz(name)) {
+		freopen((name+".in").c_str(), "r", stdin);
+		freopen((name+".out").c_str(), "w", stdout);
+	}
+}
+
+class Solution {
+public:
+  int longestPalindrome(string s) {
+    map<char, int> freq;
+    each(i, s) {
+      if(freq.find(i) == freq.end()) {
+        freq[i] = 0;
+      }
+      freq[i] += 1;
+    }
+
+    int ret = 0;
+    each(i, freq) {
+      ret += (i.s / 2) * 2;
+    }
+
+    if(sz(s) > ret) {
+      ret += 1;
+    }
+    return ret;
+  }
+};

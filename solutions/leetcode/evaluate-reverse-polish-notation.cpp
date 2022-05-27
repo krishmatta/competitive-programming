152f642be732e5258d17aaa4d36480fe95d8c08b
Krish Matta <self@krishxmatta.dev>
2022-05-27 13:46:12 -0400

    Solve leetcode/evaluate-reverse-polish-notation

---
+++ solutions/leetcode/evaluate-reverse-polish-notation.cpp
@@ -0,0 +1,58 @@
+using ll = long long;
+using db = long double;
+using str = string;
+
+using vi = vector<int>;
+#define pb push_back
+#define all(x) begin(x), end(x)
+#define sz(x) (int)(x).size()
+
+using pi = pair<int,int>;
+#define f first
+#define s second
+#define mp make_pair
+
+#define FOR(i, a, b) for (int i = (a); i < (b); ++i)
+#define F0R(i, a) FOR(i, 0, a)
+#define ROF(i, a, b) for (int i = (b) - 1; i >= (a); --i)
+#define R0F(i, a) ROF(i, 0, a)
+#define rep(a) F0R(_, a)
+#define each(a, x) for (auto& a: x)
+
+const int dr[4] = {1, 0, -1, 0}, dc[4] = {0, 1, 0, -1};
+
+const int MOD = 1e9+7;
+const int INF = 1e9;
+
+void setIO(string name = "") {
+	cin.tie(0)->sync_with_stdio(0);
+  if (sz(name)) {
+		freopen((name+".in").c_str(), "r", stdin);
+		freopen((name+".out").c_str(), "w", stdout);
+	}
+}
+
+class Solution {
+public:
+  int evalRPN(vector<string>& tokens) {
+    deque<int> stack;
+    each(i, tokens) {
+      if(i[sz(i) - 1] - '0' >= 0) {
+        stack.pb(stoi(i));
+      } else {
+        int b = stack.back(); stack.pop_back();
+        int a = stack.back(); stack.pop_back();
+        if(i[0] == '+') {
+          stack.pb(a + b);
+        } else if(i[0] == '-') {
+          stack.pb(a - b);
+        } else if(i[0] == '*') {
+          stack.pb(a * b);
+        } else if(i[0] == '/') {
+          stack.pb(a / b);
+        }
+      }
+    }
+    return stack.back();
+  }
+};

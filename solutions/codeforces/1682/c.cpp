fd02ab2a87ed8efb547b67101736651a6c45a069
Krish Matta <self@krishxmatta.dev>
2022-05-28 13:05:47 -0400

    Solve codeforces/1682/c

---
+++ solutions/codeforces/1682/c.cpp
@@ -0,0 +1,65 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+using ll = long long;
+using db = long double;
+using str = string;
+
+using vi = vector<int>;
+#define pb push_back
+#define all(x) begin(x), end(x)
+#define sz(x) (int)(x).size()
+
+using pi = pair<int,int>;
+#define f first
+#define s second
+#define mp make_pair
+
+#define FOR(i, a, b) for (int i = (a); i < (b); ++i)
+#define F0R(i, a) FOR(i, 0, a)
+#define ROF(i, a, b) for (int i = (b) - 1; i >= (a); --i)
+#define R0F(i, a) ROF(i, 0, a)
+#define rep(a) F0R(_, a)
+#define each(a, x) for (auto& a: x)
+
+const int dr[4] = {1, 0, -1, 0}, dc[4] = {0, 1, 0, -1};
+
+const int MOD = 1e9+7;
+const int INF = 1e9;
+
+void setIO(string name = "") {
+	cin.tie(0)->sync_with_stdio(0);
+  if (sz(name)) {
+		freopen((name+".in").c_str(), "r", stdin);
+		freopen((name+".out").c_str(), "w", stdout);
+	}
+}
+
+int main() {
+  setIO();
+
+  int t; cin >> t;
+  while(t) {
+    int n; cin >> n;
+    map<int, int> freq;
+    rep(n) {
+      int x; cin >> x;
+      if(freq.find(x) == freq.end()) {
+        freq[x] = 0;
+      }
+      freq[x] += 1;
+    }
+
+    int one = 0;
+    int two = 0;
+    each(i, freq) {
+      if(i.s >= 2) {
+        two += 1;
+      } else {
+        one += 1;
+      }
+    }
+    cout << two + ((one + 1) / 2) << "\n";
+    t--;
+  }
+}

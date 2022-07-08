da80f93664e2609166e4be93b8e0b791f9989425
Krish Matta <self@krishxmatta.dev>
2022-07-07 23:08:01 -0700

    Solve codeforces/1692/f

---
+++ solutions/codeforces/1692/f.cpp
@@ -0,0 +1,76 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+using ll = long long;
+using db = long double;
+using str = string;
+
+using vi = vector<int>;
+#define pb push_back
+#define all(x) begin(x), end(x)
+#define sz(x) (int)(x).size()
+
+using pi = pair<int,int>;
+#define f first
+#define s second
+#define mp make_pair
+
+#define FOR(i, a, b) for (int i = (a); i < (b); ++i)
+#define F0R(i, a) FOR(i, 0, a)
+#define ROF(i, a, b) for (int i = (b) - 1; i >= (a); --i)
+#define R0F(i, a) ROF(i, 0, a)
+#define rep(a) F0R(_, a)
+#define each(a, x) for (auto& a: x)
+
+#define YES cout << "YES" << "\n";
+#define NO cout << "NO" << "\n";
+
+const int dr[4] = {1, 0, -1, 0}, dc[4] = {0, 1, 0, -1};
+
+const int MOD = 1e9+7;
+const int INF = 1e9;
+
+void setIO(string name = "") {
+	cin.tie(0)->sync_with_stdio(0);
+  if (sz(name)) {
+		freopen((name+".in").c_str(), "r", stdin);
+		freopen((name+".out").c_str(), "w", stdout);
+	}
+}
+
+int main() {
+  setIO();
+
+  int t; cin >> t;
+  while(t) {
+    int n; cin >> n;
+    vi count(10, 0);
+    vi a;
+    rep(n) {
+      int x; cin >> x; x %= 10;
+      if(count[x] < 3) {
+        count[x]++;
+        a.pb(x);
+      }
+    }
+
+    bool ret = false;
+    F0R(i, sz(a)) {
+      FOR(j, i + 1, sz(a)) {
+        FOR(k, j + 1, sz(a)) {
+          if((a[i] + a[j] + a[k]) % 10 == 3) {
+            ret = true;
+          }
+        }
+      }
+    }
+
+    if(ret) {
+      YES
+    } else {
+      NO
+    }
+
+    t--;
+  }
+}

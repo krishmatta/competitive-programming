2aa069c295b774373acd8921b771c3c49b94b72f
Krish Matta <self@krishxmatta.dev>
2022-07-10 23:13:32 -0700

    Solve codeforces/1700/b

---
+++ solutions/codeforces/1700/b.cpp
@@ -0,0 +1,86 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+using ll = long long;
+using db = long double;
+using str = string;
+
+using vi = vector<int>;
+#define pb push_back
+#define all(x) begin(x), end(x)
+#define sz(x) (int)(x).size()
+
+using pi = pair<int,int>;
+#define f first
+#define s second
+#define mp make_pair
+
+#define FOR(i, a, b) for (int i = (a); i < (b); ++i)
+#define F0R(i, a) FOR(i, 0, a)
+#define ROF(i, a, b) for (int i = (b) - 1; i >= (a); --i)
+#define R0F(i, a) ROF(i, 0, a)
+#define rep(a) F0R(_, a)
+#define each(a, x) for (auto& a: x)
+
+#define YES cout << "YES" << "\n";
+#define NO cout << "NO" << "\n";
+
+#define tst int t; cin >> t; while(t--)
+
+const int dr[4] = {1, 0, -1, 0}, dc[4] = {0, 1, 0, -1};
+
+const int MOD = 1e9+7;
+const int INF = 1e9;
+
+void vin(int n, vi& a) {
+  rep(n) {
+    int ai; cin >> ai;
+    a.pb(ai);
+  }
+}
+
+void setIO(string name = "") {
+	cin.tie(0)->sync_with_stdio(0);
+  if (sz(name)) {
+		freopen((name+".in").c_str(), "r", stdin);
+		freopen((name+".out").c_str(), "w", stdout);
+	}
+}
+
+str sub(str a, str b) {
+  str ret = "";
+  int carry = 0;
+  R0F(i, sz(b)) {
+    int ai = a[i] - '0' + carry;
+    int bi = b[i] - '0';
+    carry = 0;
+    if(ai < bi) {
+      carry = -1;
+      ret += (char) ((10 + ai) - bi) + '0';
+    } else {
+      ret += (char) (ai - bi) + '0';
+    }
+  }
+  reverse(all(ret));
+  return ret;
+}
+
+int main() {
+  setIO();
+
+  tst {
+    int n; cin >> n;
+    str s; cin >> s;
+    str ret = "";
+    if(s[0] - '0' == 9) {
+      rep(sz(s) + 1) {
+        ret += '1';
+      }
+    } else {
+      rep(sz(s)) {
+        ret += '9';
+      }
+    }
+    cout << sub(ret, s) << "\n";
+  }
+}

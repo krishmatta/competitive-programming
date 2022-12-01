799923d393779fc8b37e4cefbc14418cf554d997
Krish Matta <self@krishxmatta.dev>
2022-12-01 02:38:15 -0500

    Solve atcoder/abc/169/d

---
+++ solutions/atcoder/abc/169/d.cpp
@@ -0,0 +1,73 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+using ll = long long;
+using db = long double;
+using str = string;
+
+using vi = vector<int>;
+#define pb push_back
+#define all(x) begin(x), end(x)
+#define sz(x) (int)(x).size()
+
+using pi = pair<int,int>;
+#define f first
+#define s second
+#define mp make_pair
+
+#define FOR(i, a, b) for (int i = (a); i < (b); ++i)
+#define F0R(i, a) FOR(i, 0, a)
+#define ROF(i, a, b) for (int i = (b) - 1; i >= (a); --i)
+#define R0F(i, a) ROF(i, 0, a)
+#define rep(a) F0R(_, a)
+#define each(a, x) for (auto& a: x)
+
+#define YES cout << "YES" << "\n";
+#define NO cout << "NO" << "\n";
+
+#define tst int t; cin >> t; while(t--)
+
+const int dr[4] = {1, 0, -1, 0}, dc[4] = {0, 1, 0, -1};
+
+const int MOD = 1e9+7;
+const int INF = 1e9;
+
+void vin(int n, vi& a) {
+  rep(n) {
+    int ai; cin >> ai;
+    a.pb(ai);
+  }
+}
+
+void setIO(string name = "") {
+	cin.tie(0)->sync_with_stdio(0);
+  if (sz(name)) {
+		freopen((name+".in").c_str(), "r", stdin);
+		freopen((name+".out").c_str(), "w", stdout);
+	}
+}
+
+int main() {
+  setIO();
+  ll n; cin >> n;
+  int ret = 0;
+  FOR(i, 2, sqrt(n) + 1) {
+    int count = 1;
+    while(n % i == 0) {
+      bool good = true;
+      rep(count) {
+        if(n % i != 0) {
+          good = false;
+          break;
+        }
+        n /= i;
+      }
+      if(good) {
+        count++;
+      }
+    }
+    ret += count - 1;
+  }
+  ret += n != 1;
+  cout << ret << "\n";
+}

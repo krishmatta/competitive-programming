b2de1d629f3b85716ce764237d33d42c39b1bf35
Krish Matta <self@krishxmatta.dev>
2022-12-23 12:47:12 -0800

    Solve atcoder/abc/282/b

---
+++ solutions/atcoder/abc/282/b.cpp
@@ -0,0 +1,78 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+using ll = long long;
+using db = long double;
+using str = string;
+
+using vi = vector<int>;
+#define pb push_back
+#define all(x) begin(x), end(x)
+#define sz(x) (int)(x).size()
+
+using pi = pair<int,int>;
+#define f first
+#define s second
+#define mp make_pair
+
+#define FOR(i, a, b) for (int i = (a); i < (b); ++i)
+#define F0R(i, a) FOR(i, 0, a)
+#define ROF(i, a, b) for (int i = (b) - 1; i >= (a); --i)
+#define R0F(i, a) ROF(i, 0, a)
+#define rep(a) F0R(_, a)
+#define each(a, x) for (auto& a: x)
+
+#define YES cout << "YES" << "\n";
+#define NO cout << "NO" << "\n";
+
+#define tst int t; cin >> t; while(t--)
+
+const int dr[4] = {1, 0, -1, 0}, dc[4] = {0, 1, 0, -1};
+
+const int MOD = 1e9+7;
+const int INF = 1e9;
+
+void vin(int n, vi& a) {
+  rep(n) {
+    int ai; cin >> ai;
+    a.pb(ai);
+  }
+}
+
+void setIO(string name = "") {
+	cin.tie(0)->sync_with_stdio(0);
+  if (sz(name)) {
+		freopen((name+".in").c_str(), "r", stdin);
+		freopen((name+".out").c_str(), "w", stdout);
+	}
+}
+
+int main() {
+  setIO();
+
+  int n, m; cin >> n >> m;
+  vi score(n, 0);
+  F0R(i, n) {
+    str si; cin >> si;
+    F0R(j, m) {
+      if(si[j] == 'o') {
+        score[i] |= 1 << (m - j - 1);
+      }
+    }
+  }
+
+  int ret = 0;
+  int perf = 0;
+  F0R(i, m) {
+    perf |= 1 << i;
+  }
+
+  F0R(i, n) {
+    FOR(j, i + 1, n) {
+      if((score[i] | score[j]) == perf) {
+        ret++;
+      }
+    }
+  }
+  cout << ret;
+}

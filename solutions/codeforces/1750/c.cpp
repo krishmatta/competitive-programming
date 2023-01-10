91dfb923c31f4052d2b2901033cdaadb9312066c
Krish Matta <self@krishxmatta.dev>
2023-01-10 03:48:10 -0800

    Solve codeforces/1750/c

---
+++ solutions/codeforces/1750/c.cpp
@@ -0,0 +1,98 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+using ll = long long;
+using db = long double;
+using str = string;
+
+using vi = vector<int>;
+#define pb push_back
+#define all(x) begin(x), end(x)
+#define sz(x) (int)(x).size()
+
+using pi = pair<int,int>;
+#define f first
+#define s second
+#define mp make_pair
+
+#define FOR(i, a, b) for (int i = (a); i < (b); ++i)
+#define F0R(i, a) FOR(i, 0, a)
+#define ROF(i, a, b) for (int i = (b) - 1; i >= (a); --i)
+#define R0F(i, a) ROF(i, 0, a)
+#define rep(a) F0R(_, a)
+#define each(a, x) for (auto& a: x)
+
+#define YES cout << "YES" << "\n";
+#define NO cout << "NO" << "\n";
+
+#define tst int t; cin >> t; while(t--)
+
+const int dr[4] = {1, 0, -1, 0}, dc[4] = {0, 1, 0, -1};
+
+const int MOD = 1e9+7;
+const int INF = 1e9;
+
+void vin(int n, vi& a) {
+  rep(n) {
+    int ai; cin >> ai;
+    a.pb(ai);
+  }
+}
+
+void setIO(string name = "") {
+	cin.tie(0)->sync_with_stdio(0);
+  if (sz(name)) {
+		freopen((name+".in").c_str(), "r", stdin);
+		freopen((name+".out").c_str(), "w", stdout);
+	}
+}
+
+int main() {
+  setIO();
+
+  tst {
+    int n; cin >> n;
+    str a; cin >> a;
+    str b; cin >> b;
+    bool same = true;
+    bool diff = true;
+    F0R(i, n) {
+      if(a[i] == b[i]) {
+        diff = false;
+      } else {
+        same = false;
+      }
+    }
+    if(!same && !diff) {
+      cout << "NO" << "\n";
+    } else {
+      cout << "YES" << "\n";
+      vector<pi> ret;
+      if(diff) {
+        ret.pb(mp(1, 1));
+        if(a[0] == '1') {
+          a[0] = '0';
+        } else {
+          a[0] = '1';
+        }
+      }
+
+      int num_one = 0;
+      F0R(i, n) {
+        if(a[i] == '1') {
+          num_one++;
+          ret.pb(mp(i + 1, i + 1));
+        }
+      }
+      if(num_one % 2) {
+        ret.pb(mp(1, 1));
+        ret.pb(mp(2, n));
+        ret.pb(mp(1, n));
+      }
+      cout << sz(ret) << "\n";
+      each(x, ret) {
+        cout << x.f << " " << x.s << "\n";
+      }
+    }
+  }
+}

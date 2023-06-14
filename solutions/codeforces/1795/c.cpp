58fbc3c1f555e130706f3f2cb2b2012251643d10
Krish Matta <self@krishxmatta.dev>
2023-06-13 21:32:54 -0700

    Solve codeforces/1795/c

---
+++ solutions/codeforces/1795/c.cpp
@@ -0,0 +1,80 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+using ll = long long;
+using db = long double;
+using str = string;
+
+using vi = vector<int>;
+#define pb push_back
+#define all(x) begin(x), end(x)
+#define sz(x) (int)(x).size()
+
+using pi = pair<int,int>;
+#define f first
+#define s second
+#define mp make_pair
+
+#define FOR(i, a, b) for (int i = (a); i < (b); ++i)
+#define F0R(i, a) FOR(i, 0, a)
+#define ROF(i, a, b) for (int i = (b) - 1; i >= (a); --i)
+#define R0F(i, a) ROF(i, 0, a)
+#define rep(a) F0R(_, a)
+#define each(a, x) for (auto& a: x)
+
+#define YES cout << "YES" << "\n";
+#define NO cout << "NO" << "\n";
+
+#define tst int t; cin >> t; while(t--)
+
+const int dr[4] = {1, 0, -1, 0}, dc[4] = {0, 1, 0, -1};
+
+const int MOD = 1e9+7;
+const int INF = 1e9;
+
+void vin(int n, vi& a) {
+  rep(n) {
+    int ai; cin >> ai;
+    a.pb(ai);
+  }
+}
+
+void setIO(string name = "") {
+	cin.tie(0)->sync_with_stdio(0);
+  if (sz(name)) {
+		freopen((name+".in").c_str(), "r", stdin);
+		freopen((name+".out").c_str(), "w", stdout);
+	}
+}
+
+int main() {
+  setIO();
+
+  tst {
+    int n; cin >> n;
+    vector<int> a, b; vin(n, a); vin(n, b);
+    vector<ll> pfix; pfix.pb(0);
+    FOR(i, 1, n + 1) {
+      pfix.pb(pfix[i - 1]);
+      pfix[i] += b[i - 1];
+    }
+
+    vector<ll> big, small; rep(n) { big.pb(0); small.pb(0); }
+    F0R(i, n) {
+      ll ai = (ll) a[i] + pfix[i];
+      ll idx = lower_bound(all(pfix), ai) - pfix.begin();
+      big[i]++;
+      if(idx - 1 >= 0 && idx - 1 < n) {
+	big[idx - 1]--;
+	small[idx - 1] += ((ll) a[i]) - (pfix[idx - 1] - pfix[i]);
+      }
+    }
+
+    ll curr = 0;
+    F0R(i, n) {
+      curr += big[i];
+      cout << (b[i] * (ll) curr + small[i]) << " ";
+    }
+    cout << "\n";
+  }
+}

a328c783b493e38da55a793064cc273426ea69a6
Krish Matta <self@krishxmatta.dev>
2023-12-25 19:42:31 -0800

    Solve cses/1081

---
+++ solutions/cses/1081.cpp
@@ -0,0 +1,78 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+using ll = long long;
+using db = long double;
+using str = string;
+
+using vi = vector<int>;
+#define pb push_back
+#define all(x) begin(x), end(x)
+#define sz(x) (int)(x).size()
+
+using pi = pair<int,int>;
+#define f first
+#define s second
+#define mp make_pair
+
+#define FOR(i, a, b) for (int i = (a); i < (b); ++i)
+#define F0R(i, a) FOR(i, 0, a)
+#define ROF(i, a, b) for (int i = (b) - 1; i >= (a); --i)
+#define R0F(i, a) ROF(i, 0, a)
+#define rep(a) F0R(_, a)
+#define each(a, x) for (auto& a: x)
+
+#define YES cout << "YES" << "\n";
+#define NO cout << "NO" << "\n";
+
+#define tst int t; cin >> t; while(t--)
+
+const int dr[4] = {1, 0, -1, 0}, dc[4] = {0, 1, 0, -1};
+
+const int MOD = 1e9+7;
+const int INF = 1e9;
+
+void vin(int n, vi& a) {
+  rep(n) {
+    int ai; cin >> ai;
+    a.pb(ai);
+  }
+}
+
+void setIO(string name = "") {
+	cin.tie(0)->sync_with_stdio(0);
+  if (sz(name)) {
+		freopen((name+".in").c_str(), "r", stdin);
+		freopen((name+".out").c_str(), "w", stdout);
+	}
+}
+
+int main() {
+  setIO();
+
+  int n; cin >> n;
+  vi ret(int(1e6)+1, 0);
+  int mx = 1;
+  rep(n) {
+    int x; cin >> x;
+    for(int i = 1; i * i <= x; i++) {
+      if((x % i) == 0) {
+        int p = i;
+        int q = x / p;
+        ret[p]++;
+        if(q != p) {
+          ret[q]++;
+        }
+
+        if(ret[p] > 1) {
+          mx = max(mx, p);
+        }
+
+        if(ret[q] > 1) {
+          mx = max(mx, q);
+        }
+      }
+    }
+  }
+  cout << mx;
+}

502f599cbd52ec3bf40bbfcaf8aea264f492bac5
Krish Matta <self@krishmatta.net>
2025-01-29 14:30:10 -0500

    Solve codeforces/2031/b

---
+++ solutions/codeforces/2031/b.cpp
@@ -0,0 +1,51 @@
+#include <bits/stdc++.h>
+using namespace std;
+using ll = long long;
+
+const int MOD = 1e9 + 7;
+const int INF = 1e9;
+
+#define all(x) x.begin(), x.end()
+#define sz(x) (int)(x).size()
+
+int main() {
+  ios::sync_with_stdio(0);
+  cin.tie(0);
+
+  int t; cin >> t;
+  while(t--) {
+    int n; cin >> n;
+
+    vector<int> p;
+    for(int i = 0; i < n; i++) {
+      int pi; cin >> pi;
+      p.push_back(pi);
+    }
+
+    bool good = true;
+    for(int i = 0; i < n; i++) {
+      int go = p[i] - 1;
+      if(abs(go - i) > 1) {
+	good = false;
+      }
+
+      if(go > i) {
+	if(p[go] != p[i] - 1) {
+	  good = false;
+	}
+      }
+
+      if(go < i) {
+	if(p[go] != p[i] + 1) {
+	  good = false;
+	}
+      }
+    }
+
+    if(good) {
+      cout << "YES\n";
+    } else {
+      cout << "NO\n";
+    }
+  }
+}
